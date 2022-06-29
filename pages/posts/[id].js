36b416b707d839928b5e10a96f66f53cbc492874
Wojciech Marek <wmarek@pgs-soft.com>
2022-06-29 09:46:20 +0200

    Init commit 2

---
+++ pages/posts/[id].js
@@ -0,0 +1,37 @@
+import React from "react";
+import Date from "../../compoents/date";
+import Layout from "../../compoents/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+const Post = ({ postData }) => {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      <Date dateString={postData.date} />
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+};
+
+export default Post;
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
